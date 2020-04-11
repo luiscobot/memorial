f5c0103e3b0a4f909bf8f9465782df9519271253
Luis Romero <diceluisco@gmail.com>
2020-04-10 22:44:34 -0500

    construir versión funcional de temporizador

---
+++ app.js
@@ -0,0 +1,23 @@
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = 0;
+            // timer = duration; // uncomment this line to reset timer automatically after reaching 0
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var time = 600 / 2, // your time in seconds here
+        display = document.querySelector('#contador');
+    startTimer(time, display);
+};
